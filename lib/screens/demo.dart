00a83b7ab7385acc20703f006756a688fccf097c
Asluf <ahmedasluf@gmail.com>
2024-01-11 15:45:51 +0530

    initial flutter commit

---
+++ lib/screens/demo.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Demo extends StatefulWidget {
+  const Demo({super.key});
+
+  @override
+  State<Demo> createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Demo page"),),
+      body: Center(child: Text("Demo page"),),
+    );
+  }
+}
